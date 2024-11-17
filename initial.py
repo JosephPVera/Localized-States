6c5aefc294f99b3f7c72c2e7d58a2574662d525a
Joseph P.Vera <148766987+JosephPVera@users.noreply.github.com>
2024-11-17 17:40:10 +0100

    Add files via upload

---
+++ initial.py
@@ -0,0 +1,17 @@
+from Localized.reader.reader import VASPXMLReader
+from Localized.analyzer.main_variables import VariablesExtractor
+
+# Read the file
+xml_reader = VASPXMLReader("vasprun.xml")
+
+# Prepare the vasprun.xml file to parse
+extractor = VariablesExtractor(xml_reader)
+
+# Find the main variables in vasprun.xml file: spin, kpoints and bands.
+extractor.find_spin_numbers()
+extractor.find_kpoint_numbers()
+extractor.find_band_numbers()
+
+print("Spin numbers:", extractor.spin_numbers)
+print("Kpoint numbers:", extractor.kpoint_numbers)
+print("Band numbers:", extractor.band_numbers)

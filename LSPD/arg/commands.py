e2e72cf64ce4c6503582664d343a7b8869432c0b
Joseph P.Vera <148766987+JosephPVera@users.noreply.github.com>
2025-04-14 22:01:51 +0200

    Update commands.py

--- LSPD/arg/commands.py
+++ LSPD/arg/commands.py
@@ -1,8 +1,6 @@
 # Written by Joseph P.Vera
 # 2025-02
 
-import argparse
-
 class CommandLineArgs:
     def __init__(self):
         self.parser = argparse.ArgumentParser(description="Generate localized defect plots.")
@@ -11,6 +9,7 @@ class CommandLineArgs:
         self.parser.add_argument('--tot', action='store_true', help="Use the 'tot' mode for plotting")
         self.parser.add_argument('--band', action='store_true', help="Display band numbers on the plot")
         self.parser.add_argument('--gamma', action='store_true', help="only for gamma calculations")
+        self.parser.add_argument('--split', action='store_true', help="split the degenerate states")
         self.args = self.parser.parse_args()
 
     @property
@@ -24,3 +23,7 @@ class CommandLineArgs:
     @property
     def gamma(self):
         return self.args.gamma
+
+    @property
+    def split_mode(self):
+        return self.args.split  
